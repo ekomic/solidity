31fd2678cda4343813b46a030e6352cc80fe8dc6
Chris Ekom <ekomchris2016@gmail.com>
2022-09-21 02:09:09 +0100

    Create constants.sol

---
+++ constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Constants {
+    // coding convention to uppercase constant variables
+    address public constant MY_ADDRESS = 0x43BcFd0cDc0C27f7F165a5F5C5ee8E9aFDd38284;
+    uint public constant MY_UINT = 123;
+}
